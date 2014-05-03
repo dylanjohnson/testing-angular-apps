1e57c77c5fe5eb1c5be34d8dee7b5a3e712bbd76
Dylan Johnson <--global>
2014-05-03 13:37:05 -0400

    Add angular and app.

---
+++ app/app.js
@@ -0,0 +1,10 @@
+/**
+ * @file Base module for the PizzaShop application
+ * @author Dylan Johnson <therealdylanjohnson@gmail.com>
+ * @copyright Dylan Johnson, 2014
+ */
+(function () {
+    'use strict';
+
+    angular.module('pizzaShop', []);
+}());
